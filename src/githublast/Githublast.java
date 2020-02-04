033358b09afa699c185a07bb9e927b20639ac5e0
jcb32@students.ptc.edu <>
2020-02-04 15:23:06 -0500

    this is the first commit

---
+++ src/githublast/Githublast.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githublast;
+
+/**
+ *
+ * @author Jarod Berger
+ */
+public class Githublast {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Berger();
+    }
+    
+    
+    public static void Berger(){
+    
+        System.out.println("Berger");
+    }
+    
+    
+}
